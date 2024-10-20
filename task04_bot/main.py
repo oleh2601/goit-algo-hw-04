192077156194a8f374630e19a91e94d605fedf67
dos_santos <nobackups@protonmail.com>
2024-10-20 15:53:01 +0300

    updated task04
    moved all the logic from main to the actual command functions

--- task04_bot/main.py
+++ task04_bot/main.py
@@ -1,4 +1,9 @@
 import sys
+def greeting():
+    print("How can I help you?")
+def farewell():
+    print("Good bye!")
+    sys.exit(0)
 #getting the command and the arguments from the user
 def parse_input(user_input):
     cmd, *args = user_input.split()
@@ -6,32 +11,43 @@ def parse_input(user_input):
     return cmd, *args
 #converting our dictionary to a string
 def get_all_contacts(contacts: dict) -> str:
-    if contacts:
-        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+    if not contacts:
+        print('No contacts found')
     else:
-        return "No contacts found."
+        print("\n".join([f"{name}: {phone}" for name, phone in contacts.items()]))
 #checking if the key exists in our dic, returning the value
-def get_phone(name, contacts: dict) -> str:
-    if name in contacts:
-        return contacts[name]
+def get_phone(*args, contacts: dict) -> str:
+    if len(args) == 1:
+        name = args[0]
+        if name in contacts:
+            print(contacts[name])
+        else:
+            print("There is no contact with such name")
     else:
-        return ("There is no contact with such name")
+        print('Wrong syntax: phone *username*')
+
     
 #changing the value of the existing item in the dictionary
-def change_contact(args, contacts: dict) -> str:
-    name, phone = args
-    contacts[name] = phone
-    return "Contact changed."
+def change_contact(*args, contacts: dict) -> str:
+    if len(args) == 2:
+        if args[0] in contacts:
+            name, phone = args
+            contacts[name] = phone
+            print("Contact changed.")
+        else:
+            print("There is no contact with such name.")
+    else:
+        print("Wrong syntax: change *username* *phone*")   
+    
 
-#adding new contact to existic dictionary
-#checking for correct arguments amount
-def add_contact(args, contacts: dict) -> str:
+
+def add_contact(*args, contacts: dict):
     if len(args) == 2:
         name, phone = args
         contacts[name] = phone
-        return "Contact added."
+        print("Contact added.")
     else:
-        return "Invalid input. Usage: add username phone"
+        print("Invalid input. Usage: add username phone")
 
 def main():
     contacts = {}
@@ -43,37 +59,17 @@ def main():
         command, *args = parse_input(user_input)
         #our only condition to exit
         if command in ["close", "exit"]:
-            print("Good bye!")
-            sys.exit(0)
+            farewell()
         elif command == "hello":
-            print("How can I help you?")
-        #for each command below checking if the user 
-        #is passing the right amount of arguments
+            greeting()
         elif command == "add":
-            if len(args) == 2:
-                print(add_contact(args, contacts))
-            else:
-                print('Wrong syntax: add *username* *phone*')        
+            add_contact(*args, contacts=contacts)
         elif command == "change":
-            if len(args) == 2:
-                if args[0] in contacts:
-                    print(change_contact(args, contacts))
-                else:
-                    print("There is no contact with such name.")
-            else:
-                print("Wrong syntax: change *username* *phone*")     
+            change_contact(*args, contacts=contacts)
         elif command == "phone":
-            if len(args) == 1:
-                print(get_phone(args[0], contacts))
-            else:
-                print('Wrong syntax: phone *username*')
+            get_phone(*args, contacts=contacts)
         elif command == "all":
-            if len(args) > 0:
-                print('Wrong syntax: all')
-            elif not contacts:
-                print('No contacts found')
-            else:
-                print(get_all_contacts(contacts))
+            get_all_contacts(contacts=contacts)
         else:
             print("Invalid command.")
 
